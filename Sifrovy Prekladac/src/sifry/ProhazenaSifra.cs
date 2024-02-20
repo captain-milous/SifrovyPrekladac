cd70d4db2403db044c22ce3c167eaf8efe405b08
captain-milous <milda.tesar3@gmail.com>
2024-02-20 14:04:05 +0100

    Update ProhazenaSifra.cs

--- Sifrovy Prekladac/src/sifry/ProhazenaSifra.cs
+++ Sifrovy Prekladac/src/sifry/ProhazenaSifra.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sifrovy_Prekladac.src.static_methods;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,37 @@ namespace Sifrovy_Prekladac.src.sifry
 {
     public class ProhazenaSifra : Sifra
     {
+        #region Konstruktory
+        public ProhazenaSifra(string rawText) : base()
+        {
+            DecText = TextMethods.WithoutDiacriticsToUpper(rawText);
+            EncText = Encrypt(DecText);
+        }
+        public ProhazenaSifra(string rawText, bool decypher) : base()
+        {
+            if (!decypher)
+            {
+                DecText = TextMethods.WithoutDiacriticsToUpper(rawText);
+                EncText = Encrypt(rawText);
+            }
+            else
+            {
+                EncText = rawText;
+                DecText = Decrypt(rawText);
+            }
+        }
+        #endregion
+        #region
+        public override string Encrypt(string text)
+        {
+            return base.Encrypt(text);
+        }
+        #endregion
+        #region
+        public override string Decrypt(string text)
+        {
+            return base.Decrypt(text);
+        }
+        #endregion
     }
 }
