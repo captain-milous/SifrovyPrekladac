b73cff4dad2fed5c739c22fabc6b32bfb6e0d973
captain-milous <milda.tesar3@gmail.com>
2024-02-21 15:00:19 +0100

    ObracenaSifra

--- Sifrovy Prekladac/src/sifry/ObracenaAbcSifra.cs
+++ Sifrovy Prekladac/src/sifry/ObracenaAbcSifra.cs
@@ -8,15 +8,15 @@ using Sifrovy_Prekladac.src.static_methods;
 namespace Sifrovy_Prekladac.src.sifry
 {
     /// <summary>
-    /// 
+    /// Třída reprezentující Obrácenou Abecední šifru, odvozená od třídy Sifra.
     /// </summary>
     public class ObracenaAbcSifra : Sifra
     {
         #region Proměnné
         /// <summary>
-        /// 
+        /// Obrácená Abeceda
         /// </summary>
-        static char[] RevAbeceda = TextMethods.Abeceda;
+        static char[] RevAbeceda = TextMethods.Abeceda.Reverse().ToArray();
         #endregion
         #region Konstruktory
         /// <summary>
@@ -25,9 +25,6 @@ namespace Sifrovy_Prekladac.src.sifry
         /// <param name="rawText"></param>
         public ObracenaAbcSifra(string rawText) : base()
         {
-            Array.Reverse(RevAbeceda);
-            Console.WriteLine(RevAbeceda.ToString());
-
             DecText = TextMethods.WithoutDiacriticsToUpper(rawText);
             EncText = Encrypt(DecText);
         }
@@ -38,7 +35,6 @@ namespace Sifrovy_Prekladac.src.sifry
         /// <param name="decypher"></param>
         public ObracenaAbcSifra(string rawText, bool decypher) : base()
         {
-            Array.Reverse(RevAbeceda);
             if (!decypher)
             {
                 DecText = TextMethods.WithoutDiacriticsToUpper(rawText);
@@ -59,7 +55,31 @@ namespace Sifrovy_Prekladac.src.sifry
         /// <returns></returns>
         public override string Encrypt(string text)
         {
-            return base.Encrypt(text);
+            StringBuilder encryptedText = new StringBuilder();
+            switch (TypeOfEnc)
+            {
+                case "DEF":
+                    foreach (char c in text)
+                    {
+                        if (TextMethods.Abeceda.Contains(c))
+                        {
+                            int index = TextMethods.FindIndexInAlphabet(c);
+                            if (index >= 0)
+                            {
+                                encryptedText.Append(RevAbeceda[index]);
+                            }
+                        }
+                        else
+                        {
+                            encryptedText.Append(c);
+                        }
+                    }
+                    return encryptedText.ToString();
+
+                default:
+                    throw new Exception("Nepodporovaný typ šifrování.");
+
+            }
         }
         #endregion
         #region dešifrování
@@ -70,8 +90,49 @@ namespace Sifrovy_Prekladac.src.sifry
         /// <returns></returns>
         public override string Decrypt(string text)
         {
-            return base.Decrypt(text);
+            StringBuilder decryptedText = new StringBuilder();
+            switch (TypeOfEnc)
+            {
+                case "DEF":
+                    foreach (char c in text)
+                    {
+                        if (RevAbeceda.Contains(c))
+                        {
+                            int index = FindIndexInRevAlphabet(c);
+                            if (index >= 0)
+                            {
+                                decryptedText.Append(TextMethods.Abeceda[index]);
+                            }
+                        }
+                        else
+                        {
+                            decryptedText.Append(c);
+                        }
+                    }
+                    return decryptedText.ToString();
+
+                default:
+                    throw new Exception("Nepodporovaný typ šifrování.");
+
+            }
         }
         #endregion
+        /// <summary>
+        /// Najde index písmenka v abecedě, která je pozpátku. Pokud ale není v této abecedě, tak vrátí -1
+        /// </summary>
+        /// <param name="input">Písmeno v abecedě</param>
+        /// <returns>Index písmena</returns>
+        private int FindIndexInRevAlphabet(char input)
+        {
+            string letter = input.ToString().ToUpper();
+            for (int i = 0; i < RevAbeceda.Length; i++)
+            {
+                if (RevAbeceda[i].ToString() == letter)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
     }
 }
