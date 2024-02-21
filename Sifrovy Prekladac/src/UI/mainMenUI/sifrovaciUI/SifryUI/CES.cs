b594936af0f7e31a22158a5fc12e78f3bfb8923a
captain-milous <milda.tesar3@gmail.com>
2024-02-21 18:48:31 +0100

    historie

--- Sifrovy Prekladac/src/UI/mainMenUI/sifrovaciUI/SifryUI/CES.cs
+++ Sifrovy Prekladac/src/UI/mainMenUI/sifrovaciUI/SifryUI/CES.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sifrovy_Prekladac.src.sifry;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +14,22 @@ namespace Sifrovy_Prekladac.src.UI.mainMenUI.sifrovaciUI.SifryUI
         /// </summary>
         /// <param name="text"></param>
         public static void Encrypt(string text)
-        {
-
+        {   
+            int input = 0;
+            while (true)
+            {
+                Console.WriteLine("Napište o kolik písmenek dopředu se má text posunout: ");
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Zadaná Hodnota musí být integer!");
+                }
+            }
+            CaesarovaSifra ces = new CaesarovaSifra(text, input, false);
         }
         /// <summary>
         /// 
---
+++ Sifrovy Prekladac/src/historie/HistoryHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sifrovy_Prekladac.src.logs;
+using Sifrovy_Prekladac.src.UserHandler;
+
+namespace Sifrovy_Prekladac.src.historie
+{
+    /// <summary>
+    /// Třída pro správu historie zpráv uživatelů.
+    /// </summary>
+    public static class HistoryHandler
+    {
+        /// <summary>
+        /// Cesta k adresáři, kde je ukládána historie zpráv.
+        /// </summary>
+        private static string historyPath = string.Empty;
+        /// <summary>
+        /// Zapíše zprávu do historie daného uživatele.
+        /// </summary>
+        /// <param name="user">Uživatel, jehož historie se má aktualizovat.</param>
+        /// <param name="text">Text zprávy k uložení do historie.</param>
+        public static void Write(User user, string text)
+        {
+            historyPath = "users\\" + user.Username + "\\historie";
+            long timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            string filePath = historyPath + "\\" + timestamp.ToString() + ".txt";
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath).Close();
+                }
+                File.AppendAllText(filePath, text);
+            }
+            catch
+            {
+                LogHandler.Write("Nastala chyba při zapsání do " + user.Username + " historie.");
+            }
+        }
+    }
+}
