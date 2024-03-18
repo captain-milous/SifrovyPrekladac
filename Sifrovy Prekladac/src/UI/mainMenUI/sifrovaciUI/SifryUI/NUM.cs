845cecdbe82aa40e82263fdff3bd6c3b3db63c17
captain-milous <milda.tesar3@gmail.com>
2024-03-18 11:05:31 +0100

    Update NUM.cs

--- Sifrovy Prekladac/src/UI/mainMenUI/sifrovaciUI/SifryUI/NUM.cs
+++ Sifrovy Prekladac/src/UI/mainMenUI/sifrovaciUI/SifryUI/NUM.cs
@@ -23,36 +23,18 @@ namespace Sifrovy_Prekladac.src.UI.mainMenUI.sifrovaciUI.SifryUI
             NumerickaSifra num = new NumerickaSifra(text, "DEF");
             while (true)
             {
-                bool wrong = false;
                 Console.WriteLine("Vyberte na jaká čísla chcete aby se text zašifrovat:");
                 Console.WriteLine("  DEF - Defaultní (Dle indexu abecedy +1); \n  BTR - Lepší (čísla 1 - 9, mají před sebou 0); \n  ROM - Římské číslice \n  SPI - Spirálové šifrování (čisla 1 - 99) \n  HEX - Hexadecimální čísla");
                 Console.Write("Vaše volba: ");
                 string answer = Console.ReadLine().ToUpper();
-                switch (answer)
+                if (answer == "DEF" || answer == "BTR" || answer == "ROM" || answer == "SPI" || answer == "HEX")
                 {
-                    case "DEF":
-                        num = new NumerickaSifra(text, answer);
-                        break;
-                    case "BTR":
-                        num = new NumerickaSifra(text, answer);
-                        break;
-                    case "ROM":
-                        num = new NumerickaSifra(text, answer);
-                        break;
-                    case "SPI":
-                        num = new NumerickaSifra(text, answer);
-                        break;
-                    case "HEX":
-                        num = new NumerickaSifra(text, answer);
-                        break;
-                    default:
-                        Console.WriteLine("Neplatná volba.");
-                        wrong = true;
-                        break;
-                }
-                if(!wrong)
-                {
+                    num = new NumerickaSifra(text, answer);
                     break;
+                }
+                else
+                {
+                    Console.WriteLine("Neplatná volba.");
                 }
             }
             HistoryHandler.Write(MainMenu.LoggedInUser, num.ToString(), ActiveSifry.Numericka_Sifra);
@@ -72,31 +54,14 @@ namespace Sifrovy_Prekladac.src.UI.mainMenUI.sifrovaciUI.SifryUI
                 Console.WriteLine("  DEF - Defaultní (Dle indexu abecedy +1); \n  BTR - Lepší (čísla 1 - 9, mají před sebou 0); \n  ROM - Římské číslice \n  SPI - Spirálové šifrování (čisla 1 - 99)");
                 Console.Write("Vaše volba: ");
                 string answer = Console.ReadLine().ToUpper();
-                switch (answer)
+                if (answer == "DEF" || answer == "BTR" || answer == "ROM" || answer == "SPI" || answer == "HEX")
                 {
-                    case "DEF":
-                        num = new NumerickaSifra(text, answer, true);
-                        break;
-                    case "BTR":
-                        num = new NumerickaSifra(text, answer, true);
-                        break;
-                    case "ROM":
-                        num = new NumerickaSifra(text, answer, true);
-                        break;
-                    case "SPI":
-                        num = new NumerickaSifra(text, answer, true);
-                        break;
-                    case "HEX":
-                        num = new NumerickaSifra(text, answer, true);
-                        break;
-                    default:
-                        Console.WriteLine("Neplatná volba.");
-                        wrong = true;
-                        break;
-                }
-                if (!wrong)
-                {
+                    num = new NumerickaSifra(text, answer, true);
                     break;
+                }
+                else
+                {
+                    Console.WriteLine("Neplatná volba.");
                 }
             }
             HistoryHandler.Write(MainMenu.LoggedInUser, num.ToString(), ActiveSifry.Numericka_Sifra);
