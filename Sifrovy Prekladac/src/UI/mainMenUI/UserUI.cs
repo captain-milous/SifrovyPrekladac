83068e405a729f500d3b25c3796e601463ad7bc8
captain-milous <milda.tesar3@gmail.com>
2024-04-02 12:12:32 +0200

    Update UserUI.cs

--- Sifrovy Prekladac/src/UI/mainMenUI/UserUI.cs
+++ Sifrovy Prekladac/src/UI/mainMenUI/UserUI.cs
@@ -9,15 +9,15 @@ using Sifrovy_Prekladac.src.UserHandler;
 namespace Sifrovy_Prekladac.src.UI.mainMenUI
 {
     /// <summary>
-    /// 
+    /// Poskytuje uživatelské rozhraní pro práci se šiframi a historií uživatele.
     /// </summary>
     public static class UserUI
     {
         /// <summary>
-        /// 
+        /// Načte seznam textových souborů z daného adresáře.
         /// </summary>
-        /// <param name="Path"></param>
-        /// <returns></returns>
+        /// <param name="Path">Cesta k adresáři, ze kterého se mají načíst textové soubory.</param>
+        /// <returns>Seznam textových souborů v daném adresáři.</returns>
         private static List<string> GetTxtFiles(string Path)
         {
             List<string> txtFiles = new List<string>();
@@ -40,9 +40,9 @@ namespace Sifrovy_Prekladac.src.UI.mainMenUI
             return txtFiles;
         }
         /// <summary>
-        /// 
+        /// Zobrazí seznam oblíbených šifer uživatele.
         /// </summary>
-        /// <param name="user"></param>
+        /// <param name="user">Uživatel, pro kterého se mají zobrazit oblíbené šifry.</param>
         public static void Favourites(User user)
         {
             string favouritePath = $"users\\{user.Username}\\favourites";
@@ -71,9 +71,9 @@ namespace Sifrovy_Prekladac.src.UI.mainMenUI
             Console.WriteLine();
         }
         /// <summary>
-        /// 
+        /// Zobrazí historii provedených šifrování uživatele.
         /// </summary>
-        /// <param name="user"></param>
+        /// <param name="user">Uživatel, pro kterého se má zobrazit historie šifrování.</param>
         public static void Historie(User user)
         {
             string historiePath = $"users\\{user.Username}\\historie";
