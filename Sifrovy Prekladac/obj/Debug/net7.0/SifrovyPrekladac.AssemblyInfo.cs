8731cc7cf1e0b90c0f030cb2cedd376d412947a2
captain-milous <milda.tesar3@gmail.com>
2024-04-02 11:54:46 +0200

    240402

--- Sifrovy Prekladac/obj/Debug/net7.0/SifrovyPrekladac.AssemblyInfo.cs
+++ Sifrovy Prekladac/obj/Debug/net7.0/SifrovyPrekladac.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SifrovyPrekladac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38364a485e54e091ad77d64779a4b17a7b5dfb0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea21b21ac81a7294e817d38bf0614209e66e6e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("SifrovyPrekladac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SifrovyPrekladac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
